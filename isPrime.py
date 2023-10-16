3eafcb3306e1a395bbe19e34c403e0133aedb004
PoornimaPDT <147930393+PoornimaPDT@users.noreply.github.com>
2023-10-16 15:41:03 +0530

    implemented example usage

--- isPrime.py
+++ isPrime.py
@@ -6,3 +6,8 @@ def is_prime(number):
             return False
     return True
 
+number = int(input("Enter a Number :"))
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
